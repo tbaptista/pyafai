55560f54df57238624b99c731874f2d944855a2e
Tiago Baptista <tiago@baptista.net>
2014-04-22 18:02:17 +0100

    [grid_example] This example now tests the get_neighbours method.

--- examples/grid_example.py
+++ examples/grid_example.py
@@ -17,12 +17,21 @@ __author__ = 'Tiago Baptista'
 import sys
 sys.path.insert(1,'..')
 
-import pyglet
 import pyafai
+import pyglet.window.mouse as mouse
+
+class MyDisplay(pyafai.Display):
+    def on_mouse_release(self, x, y, button, modifiers):
+        super(MyDisplay, self).on_mouse_release(x, y, button, modifiers)
+
+        if button == mouse.LEFT:
+            x1, y1 = self.world.get_cell(x, y)
+            print(self.world.get_neighbours(x1, y1))
+
 
 def main():
     world = pyafai.World2DGrid(10, 10, 30, True)
-    display = pyafai.Display(world)
+    display = MyDisplay(world)
 
     obj = pyafai.Object(20, 30)
     shape = pyafai.shapes.Circle(10, color=('c3B', (180, 0, 0)))
